7ba99cac4bd3cf453ef521e668f90b959b31b58c
alejandro <alejandro.tadeo.romero@gmail.com>
2020-07-23 15:36:32 +0200

    README

---
+++ blog/database/migrations/2020_07_23_073705_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('author_id')->unsigned()->index();
+
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+
+            $table->string('title');
+
+            $table->text('description');
+
+            $table->longText('content');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
