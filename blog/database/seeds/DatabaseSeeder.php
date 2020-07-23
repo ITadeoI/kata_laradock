6f7178be9851b669a1fa72c758a7bb2750c07145
alejandro <alejandro.tadeo.romero@gmail.com>
2020-07-23 13:43:14 +0200

    fixed bug with db seed table

--- blog/database/seeds/DatabaseSeeder.php
+++ blog/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AuthorSeeder::class);
+//        $this->call(AuthorSeeder::class);
 
         $this->call(PostSeeder::class);
     }
